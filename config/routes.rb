42c88a1cf2ecae2066bbd86dfc30f76260c6b357
syounin <abedeskedo@gmail.com>
2024-04-15 18:43:14 +0900

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'prototypes#index'
+  resources :prototypes, only: [:new, :create, :show, :edit, :update, :destroy] do
+   resources :comments, only: :create
   
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  end
 
-  # Defines the root path route ("/")
-  # root "messages#index"
+  resources :users, only: :show
+
 end
